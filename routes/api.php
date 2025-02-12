1213ed1bc61c578b10c9a250efa15a32661fb8c9
sufmbra <yusufumbara03@gmail.com>
2025-02-12 12:08:28 +0700

    wisata-idn

--- routes/api.php
+++ routes/api.php
@@ -1,14 +1,22 @@
+
 <?php
 use App\Http\Controllers\Api\AuthController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
-use Illuminate\Http\Request;
+use App\Http\Controllers\CategoryController;
 
-Route::post('/register', [AuthController::class, 'register']);
-Route::get('/user', [UserController::class, 'index']);
-Route::get ('/login', [AuthController::class, 'login']);
-Route::post ('/login', [AuthController::class, 'login']);
-Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
-Route::middleware('auth:sanctum')->group( function () {
-    Route::post('/logout', [AuthController::class, 'logout']);
+//! auth
+Route::post('/register', [AuthController::class, 'register']); //* ini untuk register
+Route::post ('/login', [AuthController::class, 'login']); //*ini untuk login
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum'); //* ini untuk logout
+Route::middleware('auth:sanctum')->get('/user', [UserController::class, 'getUser']); //*ini untuk mengembil data user yg sedang login
+
+
+//!category
+Route::middleware('auth:sanctum')->prefix('categories')->group(function () {
+    Route::get('/', [CategoryController::class, 'index']);
+    Route::post('/', [CategoryController::class, 'store']);
+    Route::get('/{id}', [CategoryController::class, 'show']);
+    Route::put('/{id}', [CategoryController::class, 'update']);
+    Route::delete('/{id}', [CategoryController::class, 'destroy']);
 });
